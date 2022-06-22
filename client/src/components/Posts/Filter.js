843462fba78236e17121f8ebe17070d2693b7c39
ziv-hagai <zivhag@gmail.com>
2022-06-22 17:04:54 +0300

    third commit

--- client/src/components/Posts/Filter.js
+++ client/src/components/Posts/Filter.js
@@ -1,45 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
 import Posts from './Posts';
 
 const Filter = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
-  const [cards, setCards] = useState();
+  const [cards, setCards] = useState(-1);
   const [filtered, setFiltered] = useState([]);
-  const [first, setFirst] = useState();
+  const [first, setFirst] = useState(true);
+
   useEffect(() => {
     setFiltered([...posts]);
   }, [posts]);
 
   const changeCards = (e) => {
     e.preventDefault();
-    setCards(Number(e.target.value));
+    setCards(e.target.value.length ? Number(e.target.value) : -1);
   };
+
   const changeFirst = (e) => {
     e.preventDefault();
-    setCards(Number(e.target.value));
+    setFirst(!first);
   };
+
   const handleSubmit = (e) => {
-    console.log(filtered);
     e.preventDefault();
-    console.log(typeof cards);
-    const newFiltered = filtered.filter((post) => post.numCreditCards === cards);
-    console.log(newFiltered);
+    const newFiltered = cards === -1 ? [...posts] : posts.filter((post) => post.numCreditCards === cards);
     setFiltered(newFiltered);
   };
 
   return (
     <div>
       <form onSubmit={(e) => handleSubmit(e)}>
-        <Stack spacing={2} direction="column">
-          <label htmlFor="cards">number of credit cards
-            <input type="number" id="cards" onChange={changeCards} />
-          </label>
-          <Button variant="contained" onClick={changeFirst}>{first ? 'And' : 'Or'}</Button>
-          <Button variant="contained" type="submit">Filter</Button>
-        </Stack>
+        {/* <Stack spacing={2} direction="column"> */}
+        <label htmlFor="cards">number of credit cards
+          <input type="number" id="cards" onChange={changeCards} />
+        </label>
+        <button type="button" onClick={changeFirst}>{first ? 'And' : 'Or'}</button>
+        <button type="submit">Filter</button>
+        {/* </Stack> */}
       </form>
       <Posts filtered={filtered} setCurrentId={setCurrentId} />
     </div>
