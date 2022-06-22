5244cc130578927ed389afdb22a47e44175697f5
ziv-hagai <zivhag@gmail.com>
2022-06-22 17:53:28 +0300

    fourth commit

--- client/src/components/Posts/Filter.js
+++ client/src/components/Posts/Filter.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Button, TextField, Select, MenuItem, InputLabel } from '@material-ui/core';
+import Posts from './Posts';
 // import Stack from '@mui/material/Stack';
 // import Button from '@mui/material/Button';
-import Posts from './Posts';
 
 const Filter = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
@@ -16,7 +17,7 @@ const Filter = ({ setCurrentId }) => {
 
   const changeCards = (e) => {
     e.preventDefault();
-    setCards(e.target.value.length ? Number(e.target.value) : -1);
+    setCards(e.target.value);
   };
 
   const changeFirst = (e) => {
@@ -33,13 +34,27 @@ const Filter = ({ setCurrentId }) => {
   return (
     <div>
       <form onSubmit={(e) => handleSubmit(e)}>
-        {/* <Stack spacing={2} direction="column"> */}
-        <label htmlFor="cards">number of credit cards
-          <input type="number" id="cards" onChange={changeCards} />
-        </label>
-        <button type="button" onClick={changeFirst}>{first ? 'And' : 'Or'}</button>
-        <button type="submit">Filter</button>
-        {/* </Stack> */}
+        <InputLabel id="cards-label">Number of Credit Cards</InputLabel>
+        <Select
+          id="cards"
+          labelId="cards-label"
+          value={cards}
+          label="cards"
+          onChange={changeCards}
+        >
+          <MenuItem value={-1}>All</MenuItem>
+          <MenuItem value={0}>0</MenuItem>
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+        </Select>
+        <TextField />
+
+        <Button type="button" onClick={changeFirst}>{first ? 'And' : 'Or'}</Button>
+        <Button type="submit">Filter</Button>
+
       </form>
       <Posts filtered={filtered} setCurrentId={setCurrentId} />
     </div>
